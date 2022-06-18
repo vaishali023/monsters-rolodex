f6aeb33757441aa5a0b7b3643e64003b317c3dda
vaishali023 <vaishalichaudhary011@gmail.com>
2022-06-18 17:34:12 -0400

    componentDidMount()
    fetching data from API

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,12 +8,25 @@ class App extends Component {
     super();
 
   this.state = {
-    monsters : [
-    
-    ]
+    monsters : []
    
   };
 }
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+   .then((response) => response.json())
+   .then((users) => 
+    this.setState(
+      () => {
+        return {monsters : users};
+      },
+      () => {
+        console.log(this.state);
+      }
+    ));
+}
+
 render() {
   return (
     <div className="App">
