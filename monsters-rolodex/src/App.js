52e83ee247729a800cac2e183068385f07320cb7
vaishali023 <vaishalichaudhary011@gmail.com>
2022-06-18 17:17:20 -0400

    Updated App.js

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,33 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor() {
+    super();
+
+  this.state = {
+    monsters : [
+    
+    ]
+   
+  };
+}
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      this.state.monsters.map((monster) => {
+        return ( <div key = {monster.id}>
+          <h1>{monster.name}</h1>
+           </div>
+        );
+        
+      })
+    }
     </div>
   );
 }
-
+}
 export default App;
