3fba4ededaa59e1d57e63e1c2bee44a448d1e303
vaishali023 <vaishalichaudhary011@gmail.com>
2022-06-23 21:36:55 -0400

    Storing original data

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     super();
 
   this.state = {
-    monsters : []
-   
+    monsters : [],
+    searchField: '',
   };
 }
 
@@ -28,25 +28,29 @@ componentDidMount() {
 }
 
 render() {
+
+  console.log('render');
+  const filteredMonsters = this.state.monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+});
+
   return (
     <div className="App">
      <input 
-     className = 'Search-box' 
+      className = 'Search-box' 
      type= 'search'
       placeholder='search monsters'
         onChange={(event) => {
           console.log(event.target.value);
-          const searchString = event.target.value.toLocaleLowerCase();
-          const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-          });
+          const searchField = event.target.value.toLocaleLowerCase();
+         
           this.setState( () => {
-            return { monsters : filteredMonsters};
+            return { searchField};
           });
         }}
       />
     {
-      this.state.monsters.map((monster) => {
+      filteredMonsters.map((monster) => {
         return ( <div key = {monster.id}>
           <h1>{monster.name}</h1>
            </div>
@@ -57,6 +61,6 @@ render() {
     }
     </div>
   );
-}
+  }
 }
 export default App;
